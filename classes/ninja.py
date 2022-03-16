dc6f260ec31d6a2ecc359d94e920f2b6958f3465
Christian Ramirez <christian.r0602@gmail.com>
2022-03-16 12:23:14 -0500

    modifying attack method on the ninja class

--- classes/ninja.py
+++ classes/ninja.py
@@ -10,5 +10,17 @@ class Ninja:
         print(f"Name: {self.name}\nStrength: {self.strength}\nSpeed: {self.speed}\nHealth: {self.health}\n")
 
     def attack( self , pirate ):
-        pirate.health -= self.strength
+        # Customize the attack methods on both the ninja and pirate
+        import random 
+        ninja_strength = random.randint(0, self.strength)
+        pirate_strength = random.randint(0, self.strength)
+        if ninja_strength > pirate_strength:
+             pirate.health -= self.strength
+             print(f'The pirate got recked! {self.health} remaining')
+        elif pirate_strength > ninja_strength:
+            print(f'The pirate took no damage {self.health} remaining')
+        else:
+            print(f'The ninja and pirate took equal amounts of damage, which cancel each other out: {self.health} remaining')
+
         return self
+
